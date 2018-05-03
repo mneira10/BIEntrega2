8d9c9e1fe0238ec13adb1adc36c2d3e9f552e833
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-05-02 22:16:18 -0500

    perfilamiento datos gerencia campus

--- perfilamiento.py
+++ perfilamiento.py
@@ -2,6 +2,6 @@ import pandas as pd
 import pandas_profiling
 
 
-df=pd.read_csv("./datosPipeline2.csv",delimiter = "|",encoding = "iso8859_2")
+df=pd.read_csv("./subirsalones_2018-1.csv",delimiter = ";",encoding = "iso8859_2")
 profile = pandas_profiling.ProfileReport(df)
-profile.to_file(outputfile="perfilamiento.html")
+profile.to_file(outputfile="perfilamientoSalonesGerenciaCampus.html")
